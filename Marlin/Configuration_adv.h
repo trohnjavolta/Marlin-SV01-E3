06a8788ee0606377ddb671c7ca35b373aa6f0470
Ondra <ondrej.morsky@hotmail.com>
2020-04-03 12:43:16 +0200

    Fixes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2077,7 +2077,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       580
+    #define Z_CURRENT       650
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
